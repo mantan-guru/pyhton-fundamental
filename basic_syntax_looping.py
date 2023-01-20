35e2392242ac057468fd4301fd75f24ce2e9a9f3
iksanh <iksanhariji@gmail.com>
2023-01-21 06:18:21 +0800

    Program repeat read book

---
+++ basic_syntax_looping.py
@@ -0,0 +1,13 @@
+"""
+Program Perulangan  membaca buku
+"""
+
+jumlah_buku = 10
+print(f'ibu berkata, "Baca semua buku"')
+
+jumlah_buku_yang_sudah_dibaca = 0
+
+for jumlah_buku_yang_sudah_dibaca in range(1,11):
+    print(f'buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca')
+
+print(jumlah_buku_yang_sudah_dibaca)
