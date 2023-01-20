b1e0026c2e056cf357893de14afccfa0dd4c16cd
iksanh <iksanhariji@gmail.com>
2023-01-20 20:37:19 +0800

    setting pep 8

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ All of the basic syntax in programing lenguange is consist of :
 3. looping  :  repeat the same steps  many times until conditions are met
 """
 
-#sequential
+# sequential
 
 print(f'ibu berkata, "pergi ke toko" ')
 print(f'budi  berkata, "Baik apa yang saya akan lakukan di toko" ')
@@ -13,3 +13,5 @@ print(f'ibu berkata, "Beli satu botol susu dan jika ada telor beli 6"')
 print(f'ibu berkata, "Beli satu botol susu dan jika ada telor beli 6"')
 print(f'maka budi  berangkat ke toko ')
 print(f'dan pergi berbelanja')
+
+# branching
