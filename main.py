d22d3cd83906a2514ec87973fd754ab5142fa0e7
iksanh <iksanhariji@gmail.com>
2023-01-20 21:12:27 +0800

    branching

--- main.py
+++ main.py
@@ -15,3 +15,18 @@ print(f'maka budi  berangkat ke toko ')
 print(f'dan pergi berbelanja')
 
 # branching
+jumlah_botol_susu = 173
+jumlah_telur = 1587
+
+print(f"Jumlah botol susu adalah  {jumlah_botol_susu} botol")
+print(f"jumlah telur adalah {jumlah_telur} butir")
+if jumlah_botol_susu > 0 :
+    print(f'Budi mengecek harganya dan ternyata uangnya cukup')
+    if jumlah_telur == 0 :
+        print(f'Budi membeli 1 botol susu')
+    else:
+        print(f'Budi membli 6 botol susu')
+else:
+    print(f'budi tidak jadi membeli 1 botol susu')
+
+print(f'budi pulang')
