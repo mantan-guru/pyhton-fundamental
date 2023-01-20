dacfa25462733926c55caf44a6a59fc6cca68ce1
iksanh <iksanhariji@gmail.com>
2023-01-20 20:33:58 +0800

    add sequential

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+All of the basic syntax in programing lenguange is consist of :
+1.  sequential  : sequetial step
+2.  branching :  jump step if condition are met
+3. looping  :  repeat the same steps  many times until conditions are met
+"""
+
+#sequential
+
+print(f'ibu berkata, "pergi ke toko" ')
+print(f'budi  berkata, "Baik apa yang saya akan lakukan di toko" ')
+print(f'ibu berkata, "Beli satu botol susu dan jika ada telor beli 6"')
+print(f'ibu berkata, "Beli satu botol susu dan jika ada telor beli 6"')
+print(f'maka budi  berangkat ke toko ')
+print(f'dan pergi berbelanja')
