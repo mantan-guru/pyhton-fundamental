063fb0cce567e975353e3764a9e7c729211482fb
iksanh <iksanhariji@gmail.com>
2023-01-21 06:24:21 +0800

    Program repeat read book using for

--- basic_syntax_looping.py
+++ basic_syntax_looping.py
@@ -7,7 +7,8 @@ print(f'ibu berkata, "Baca semua buku"')
 
 jumlah_buku_yang_sudah_dibaca = 0
 
-for jumlah_buku_yang_sudah_dibaca in range(1,11):
+#use for to repeate the statemen
+for jumlah_buku_yang_sudah_dibaca in range(1,jumlah_buku + 1):
     print(f'buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca')
 
 print(jumlah_buku_yang_sudah_dibaca)
