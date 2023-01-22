66e50ae16f53e24af4bcf14de6080eabd7f4f7ef
iksanh <iksanhariji@gmail.com>
2023-01-22 08:23:17 +0800

    short variabel name

--- basic_syntax_looping_while_with_undifined_count.py
+++ basic_syntax_looping_while_with_undifined_count.py
@@ -2,24 +2,24 @@
 Program Perulangan  membaca buku dengan while
 """
 
-jumlah_buku = 10
+buku = 10
 print(f'ibu berkata, "Baca semua buku"')
 
-jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
-jumlah_total_baca = 0
+jumlah_paham = 0
+jumlah_baca = 0
 
 #use while to repeate the statemen
-while jumlah_total_baca < jumlah_buku * 2  :
-    jumlah_total_baca = jumlah_total_baca + 1
-    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9 :
-        print(f'buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami +  1 } belum paham')
+while jumlah_baca < buku * 2  :
+    jumlah_baca = jumlah_baca + 1
+    if jumlah_paham == 9 :
+        print(f'buku ke {jumlah_paham + 1 } belum paham')
     else :
-        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
-        print(f'buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami')
+        jumlah_paham = jumlah_paham + 1
+        print(f'buku ke {jumlah_paham} sudah dibaca dan dipahami')
 
-print(f'jumlah_buku_yang_sudah_dibaca_dan_dipahami adalah {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
-if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku:
+print(f'jumlah_buku_yang_sudah_dibaca_dan_dipahami adalah {jumlah_paham}')
+if jumlah_paham == buku:
     print('Bu, semua buku bisa dipahami')
 else :
-    print(f'Bu, tidak semua buku dipahami, budi hanya bisa memahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku')
+    print(f'Bu, tidak semua buku dipahami, budi hanya bisa memahami {jumlah_paham} buku')
 
